2a1fa84cf3e59b0ad710aa520b4049cece496c46
Ismael Hosen <ismail27.dec@gmail.com>
2021-06-29 20:11:42 +0600

    Swagger Integrated

--- ChatApp.WebAPI/Startup.cs
+++ ChatApp.WebAPI/Startup.cs
@@ -26,16 +26,22 @@ namespace ChatApp.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseHttpsRedirection();
 
             app.UseRouting();
