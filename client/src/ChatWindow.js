8d45f93e2abf87697be64faa26f2cbd917f06d06
Ismael Hosen <ismail27.dec@gmail.com>
2021-06-29 21:32:03 +0600

    Chat Applicaiton Works on Real-Time

--- client/src/ChatWindow.js
+++ client/src/ChatWindow.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Message from './Message/Message';
+import Message from './Message';
 
 const ChatWindow = (props) => {
     const chat = props.chat.map(m => <Message
